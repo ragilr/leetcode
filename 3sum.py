57f73ae32f4cf64a368fa199f8d6b5d3dc637caa
ragilr <ragilr@gmail.com>
2021-11-28 11:57:15 +0530

    3 sum

--- 3sum.py
+++ 3sum.py
@@ -1,12 +1,11 @@
 class Solution:
-    def threeSum(self, nums: List[int]) -> List[List[int]]:
+    #functions with 1 in suffix is hashmap implementation
+    def threeSum1(self, nums: List[int]) -> List[List[int]]:
         ans = []
         d = dict()
-        zc = 0
         for x in range(0,len(nums)):
             n = nums[x]
-            if n==0:
-                zc += 1
+            if( x > 0 and nums[x-1]==n):
                 continue
             it = self.twoSums(x,nums,-n)
             if(len(it)>0):
@@ -18,11 +17,40 @@ class Solution:
                         d[tuple(cpy)]=1
                     
         # return [self.twoSums([1,2,3],5)]
-        if zc >= 3:
-            ans.append([0,0,0])
         return ans
     
-    def twoSums(self, avoid:int,nums: List[int], target:int) -> List[int]:
+    def threeSum(self, nums: List[int]) -> List[List[int]]:
+        ans = []
+        nums.sort()
+        print(nums)
+        for i in range(len(nums)):
+            if(nums[i]>0):
+                break
+            if(i==0 or nums[i]!=nums[i-1]):
+                self.twoSums(i,nums,ans)
+        return ans
+    
+    def twoSums(self, x:int, nums:List[int], res: List[List[int]]) -> List[int]:
+        l = x+1
+        r = len(nums)-1
+        while(l<r):
+            sum = nums[x]+nums[l]+nums[r]
+            if(sum==0):
+                res.append([nums[x],nums[l],nums[r]])
+                l +=1
+                r -=1
+                while l<r and nums[l]==nums[l-1]:
+                    l+=1
+            elif sum>0:
+                r-=1
+            else:
+                l+=1
+                
+        return res
+                
+        
+    
+    def twoSums1(self, avoid:int,nums: List[int], target:int) -> List[int]:
         d = dict()
         ans = []
         for i in range(0,len(nums)):
