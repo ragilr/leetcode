0736c157a1ef113ce74af36af8b7c34cbc1203c7
ragilr <ragilr@gmail.com>
2022-02-01 23:58:12 +0530

    feb contest1

--- README.md
+++ README.md
@@ -89,3 +89,4 @@ solutions on leetcode
 |84|number-of-unique-flavors-after-sharing-k-candies|https://leetcode.com/problems/number-of-unique-flavors-after-sharing-k-candies|
 |85|max-consecutive-ones-iii|https://leetcode.com/problems/max-consecutive-ones-iii|
 |86|number-of-substrings-containing-all-three-characters|https://leetcode.com/problems/number-of-substrings-containing-all-three-characters|
+|87|best-time-to-buy-and-sell-stock|https://leetcode.com/problems/best-time-to-buy-and-sell-stock|
---
+++ best-time-to-buy-and-sell-stock.py
@@ -0,0 +1,20 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        low=[0]*len(prices)
+        high=[0]*len(prices)
+        l=prices[0]
+        for i in range(len(prices)):
+            if prices[i]<l:
+                l=prices[i]
+            low[i]=l
+        h=prices[-1]
+        for i in range(len(prices)-1,-1,-1):
+            if prices[i]>h:
+                h=prices[i]
+            high[i]=h
+        m=0
+        for i in range(len(prices)):
+            if m < high[i]-low[i]:
+                m=high[i]-low[i]    
+        return m
+    
