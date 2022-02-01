87e308452b4270817ca105c81b91a25f8c6453f1
ragilr <ragilr@gmail.com>
2022-02-02 00:05:57 +0530

    stocks optimized

--- best-time-to-buy-and-sell-stock.py
+++ best-time-to-buy-and-sell-stock.py
@@ -1,20 +1,29 @@
 class Solution:
-    def maxProfit(self, prices: List[int]) -> int:
-        low=[0]*len(prices)
-        high=[0]*len(prices)
-        l=prices[0]
-        for i in range(len(prices)):
-            if prices[i]<l:
-                l=prices[i]
-            low[i]=l
-        h=prices[-1]
-        for i in range(len(prices)-1,-1,-1):
-            if prices[i]>h:
-                h=prices[i]
-            high[i]=h
-        m=0
-        for i in range(len(prices)):
-            if m < high[i]-low[i]:
-                m=high[i]-low[i]    
-        return m
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     low=[0]*len(prices)
+    #     high=[0]*len(prices)
+    #     l=prices[0]
+    #     for i in range(len(prices)):
+    #         if prices[i]<l:
+    #             l=prices[i]
+    #         low[i]=l
+    #     h=prices[-1]
+    #     for i in range(len(prices)-1,-1,-1):
+    #         if prices[i]>h:
+    #             h=prices[i]
+    #         high[i]=h
+    #     m=0
+    #     for i in range(len(prices)):
+    #         if m < high[i]-low[i]:
+    #             m=high[i]-low[i]    
+    #     return m
     
+    def maxProfit(self, prices: List[int]) -> int:
+        l = prices[0]
+        m = 0
+        for p in prices:
+            if p < l:
+                l=p
+            if p-l >m:
+                m=p-l
+        return m
