7f3f5512bfeda69cde0870828ab86fb2f30e04cc
ragil <ragilr@gmail.com>
2023-08-01 01:18:33 +0530

    Optimize search in rotated sorted array

--- search-in-rotated-sorted-array.py
+++ search-in-rotated-sorted-array.py
@@ -37,6 +37,28 @@ class Solution:
         if(left!=None):
             return left
         return self.findPivot(mid,h,nums)
+    
+class Solution:
+    def search(self, nums: List[int], target: int) -> int:
+        l = 0
+        h = len(nums)-1
+        print(nums)
+        while l <= h:
+            mid = (l+h)//2
+            print(l,h,mid)
+            if nums[mid]==target:
+                return mid
+            if nums[l]<=nums[mid]:
+                if nums[l]<= target and target<=nums[mid]:
+                    h=mid-1
+                else:
+                    l=mid+1
+            else:
+                if target<=nums[h] and nums[mid]<=target:
+                    l=mid+1
+                else:
+                    h=mid-1
+        return -1
 
         
         
