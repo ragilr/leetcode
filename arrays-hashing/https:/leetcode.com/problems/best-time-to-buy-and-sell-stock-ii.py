a5535ff8a96daa479263127c34e765c23f7dfb40
ragil <ragilr@gmail.com>
2023-09-24 20:05:18 +0530

    Max profit buy sells stock optimized

--- arrays-hashing/https:/leetcode.com/problems/best-time-to-buy-and-sell-stock-ii.py
+++ arrays-hashing/https:/leetcode.com/problems/best-time-to-buy-and-sell-stock-ii.py
@@ -1,15 +1,19 @@
 class Solution:
+    # def maxProfit(self, prices: List[int]) -> int:
+    #     buy = prices[0]
+    #     profit = 0
+    #     prices.append(-1)
+    #     for i in range(1,len(prices)):
+    #         price = prices[i]
+    #         if price < prices[i-1]:
+    #             if prices[i-1]-buy > 0:
+    #                 profit += (prices[i-1]-buy)
+    #             buy = price
+    #     return profit
+    
     def maxProfit(self, prices: List[int]) -> int:
-        buy = prices[0]
         profit = 0
-        sold = set()
         for i in range(1,len(prices)):
-            price = prices[i]
-            if price < prices[i-1]:
-                if prices[i-1]-buy > 0:
-                    profit += (prices[i-1]-buy)
-                    sold.add(i)
-                buy = price
-        if len(prices)-1 not in sold and buy<prices[-1]:
-            profit = profit+prices[-1]-buy
+            if prices[i] > prices[i-1]:
+                    profit += (prices[i]-prices[i-1])
         return profit
