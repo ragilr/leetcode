4d3962d4239a65041d55e188d8f87ec52ba3116f
ragil <ragilr@gmail.com>
2024-02-04 23:32:06 +0530

    sort colrs

--- arrays-hashing/sort-colors.py
+++ arrays-hashing/sort-colors.py
@@ -3,25 +3,12 @@ class Solution:
         """
         Do not return anything, modify nums in-place instead.
         """
-        count=[0,0,0]
-        for c in nums:
-            count[c]+=1
-        i=0
-        n=count[0]
-        while n>0:
-            nums[i]=0
-            n-=1
-            i+=1
-        n=count[1]
-        while n>0:
-            nums[i]=1
-            n-=1
-            i+=1
-        n=count[2]
-        while n>0:
-            nums[i]=2
-            n-=1
-            i+=1
-        
-        
-        
+        count = Counter(nums)
+        j = 0
+        for i in range(3):
+            n = count[i]
+            while n:
+                nums[j]=i
+                n=n-1
+                j+=1
+        return nums
