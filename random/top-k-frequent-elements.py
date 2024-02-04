09f058cffa1f61cd9e094a25adb661c5e7b510a9
ragil <ragilr@gmail.com>
2024-02-04 14:16:35 +0530

    top k freq elements

--- random/top-k-frequent-elements.py
+++ random/top-k-frequent-elements.py
@@ -1,18 +1,14 @@
 class Solution:
     def topKFrequent(self, nums: List[int], k: int) -> List[int]:
-        d =dict()
-        for n in nums:
-            d[n]=d.get(n,0)+1
-        freq = []
-        for key,value in d.items():
-            freq.append([value,key])
-        freq.sort(key = lambda x:x[0])
+        c = Counter(nums)
+        freq = [[] for i in range(len(nums)+1)]
+        # print(freq,c)
+        for i,v in c.items():
+            freq[v].append(i)
+        ret = []
         # print(freq)
-        ret=[]
-        i = len(freq)-1
-        while k>0:
-            ret.append(freq[i][1])
-            k-=1
-            i-=1
-        return ret
-        
+        for i in range(len(freq)-1,0,-1):
+            for n in freq[i]:
+                ret.append(n)
+                if k==len(ret):
+                    return ret
