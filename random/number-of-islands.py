eb09ef76140ea09f378afcefb23db1eb5f788b33
ragil <ragilr@gmail.com>
2024-02-20 02:31:57 +0530

    num of islands redo

--- random/number-of-islands.py
+++ random/number-of-islands.py
@@ -1,30 +1,24 @@
 class Solution:
-
-    
     def numIslands(self, grid: List[List[str]]) -> int:
-        def isValid(i,j) -> bool:
-            return (i>-1 and i<len(grid) and j>-1 and j<len(grid[0]))
-        def count(i,j):
+        count = 0
+        row = len(grid)
+        col = len(grid[0])
+        def dfs(i,j):
+            if i<0 or i>=row or j<0 or j>=col:
+                return
+            if grid[i][j]=="0":
+                return
             grid[i][j]="0"
-            # print("hey",i,j)
-            if isValid(i+1,j) and grid[i+1][j]=="1":
-                count(i+1,j)
-            if isValid(i-1,j) and grid[i-1][j]=="1":
-                count(i-1,j)
-            if isValid(i,j+1) and grid[i][j+1]=="1":
-                count(i,j+1)
-            if isValid(i,j-1) and grid[i][j-1]=="1":                
-                count(i,j-1)
-            return 1
-        c=0
-        print("limits",len(grid),len(grid[0]))
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                # print(grid)
+            # print(i,j)
+            dfs(i-1,j)
+            dfs(i+1,j)
+            dfs(i,j+1)
+            dfs(i,j-1)
+
+        for i in range(row):
+            for j in range(col):
                 if grid[i][j]=="1":
-                    c+=1
-                    count(i,j)
-                    # print("herer",i,j,c)
-        return c
-                
-                
+                    dfs(i,j)
+                    count += 1
+                    # print(grid)
+        return count
